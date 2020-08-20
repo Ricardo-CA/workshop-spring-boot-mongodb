75fa015a2f39ed46453d232c99fd53e51abe8c55
Ricardo Carvalho <ricorino1@gmail.com>
2020-08-20 11:18:44 -0300

    User delete with DELETE method

--- src/main/java/com/ricadoca/workshopmongo/services/UserService.java
+++ src/main/java/com/ricadoca/workshopmongo/services/UserService.java
@@ -34,4 +34,7 @@ public class UserService {
 		
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	public void delete(String id) {
+		userRepository.deleteById(id);
+	}
 }
