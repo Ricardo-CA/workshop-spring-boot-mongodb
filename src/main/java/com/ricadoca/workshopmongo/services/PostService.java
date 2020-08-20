7ffdb1692d8b2050aa4095557e76941d5cded202
Ricardo Carvalho <ricorino1@gmail.com>
2020-08-20 17:21:11 -0300

    GET post by id

---
+++ src/main/java/com/ricadoca/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.ricadoca.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricadoca.workshopmongo.domain.Post;
+import com.ricadoca.workshopmongo.repository.PostRepository;
+import com.ricadoca.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
