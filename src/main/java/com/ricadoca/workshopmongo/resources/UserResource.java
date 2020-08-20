889ac6f56e928ae46b6b2aec5a9eb2d17b386b62
Ricardo Carvalho <ricorino1@gmail.com>
2020-08-20 17:13:25 -0300

    User posts endpoint

--- src/main/java/com/ricadoca/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ricadoca/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ricadoca.workshopmongo.domain.Post;
 import com.ricadoca.workshopmongo.domain.User;
 import com.ricadoca.workshopmongo.dto.UserDTO;
 import com.ricadoca.workshopmongo.services.UserService;
@@ -67,5 +68,13 @@ public class UserResource {
 		obj.setId(id);
 		service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
